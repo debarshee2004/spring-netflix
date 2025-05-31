494530bcbea449494a79e43e8571f0c4a7c292b6
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-06-01 01:44:05 +0530

    Add LOLOMO service - for home screen

---
+++ services/spring-lolomo-service/src/main/java/com/netflix/spring_lolomo_service/SpringLolomoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.spring_lolomo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLolomoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLolomoServiceApplication.class, args);
+	}
+
+}
---
+++ services/spring-lolomo-service/src/test/java/com/netflix/spring_lolomo_service/SpringLolomoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.spring_lolomo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLolomoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

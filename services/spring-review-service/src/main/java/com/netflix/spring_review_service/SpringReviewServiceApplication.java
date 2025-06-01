1073237f812570de7950ce37a34bb577c64e1714
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-06-01 20:27:49 +0530

    Created review service

--- services/spring-review-service/src/main/java/com/netflix/spring_review_service/SpringReviewServiceApplication.java
+++ services/spring-review-service/src/main/java/com/netflix/spring_review_service/SpringReviewServiceApplication.java
@@ -1,4 +1,4 @@
-package com.netflix.springreviewservice;
+package com.netflix.spring_review_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringReviewServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringReviewServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReviewServiceApplication.class, args);
+	}
 
 }
--- services/spring-review-service/src/test/java/com/netflix/spring_review_service/SpringReviewServiceApplicationTests.java
+++ services/spring-review-service/src/test/java/com/netflix/spring_review_service/SpringReviewServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.netflix.springreviewservice;
+package com.netflix.spring_review_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringReviewServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
